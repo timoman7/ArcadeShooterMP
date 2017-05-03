df849b462a5dd7584e84fe5fd7d421aa186d9f8a
timoman7 <tc20791@sestudent.org>
2017-05-03 11:31:58 -0400

    Update login.js

--- login.js
+++ login.js
@@ -16,46 +16,71 @@ setInterval(function(){
 function initApp(){
 	googleProvider = new firebase.auth.GoogleAuthProvider();
 	googleProvider.addScope('https://www.googleapis.com/auth/plus.login');
-	firebase.auth().getRedirectResult().then(function(result) {
-		//Log in
-		var user = result.user;
-		var credential = result.credential;
-		if(user===null){
-			//User is null after redirect
-		}else{
-			//User is defined after redirect
-			currentUser = firebase.auth().currentUser;
-		}
-	}, function(error) {
-		// The provider's account email, can be used in case of
-		// auth/account-exists-with-different-credential to fetch the providers
-		// linked to the email:
-		var email = error.email;
-		// The provider's credential:
-		var credential = error.credential;
-		// In case of auth/account-exists-with-different-credential error,
-		// you can fetch the providers using this:
-		console.log(error);
-
-	});
-	firebase.auth().onAuthStateChanged(function(user) {
-		console.log(user);
-		if(user){
-			//Already logged in
-			currentUser = firebase.auth().currentUser;
-		}else{
-
-		}
-	},function(err){
-		console.log(err);
-	});
+	// Result from Redirect auth flow.
+      // [START getidptoken]
+      firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // [START_EXCLUDE]
+          document.getElementById('quickstart-oauthtoken').textContent = token;
+        } else {
+          document.getElementById('quickstart-oauthtoken').textContent = 'null';
+          // [END_EXCLUDE]
+        }
+        // The signed-in user info.
+        var user = result.user;
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // [START_EXCLUDE]
+        if (errorCode === 'auth/account-exists-with-different-credential') {
+          alert('You have already signed up with a different auth provider for that email.');
+          // If you are using multiple auth providers on your app you should handle linking
+          // the user's accounts here.
+        } else {
+          console.error(error);
+        }
+        // [END_EXCLUDE]
+      });
+      // [END getidptoken]
+      // Listening for auth state changes.
+      // [START authstatelistener]
+      firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          var displayName = user.displayName;
+          var email = user.email;
+          var emailVerified = user.emailVerified;
+          var photoURL = user.photoURL;
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          var providerData = user.providerData;
+          // [START_EXCLUDE]
+          currentUser = firebase.auth().currentUser;
+          // [END_EXCLUDE]
+        } else {
+          // User is signed out.
+          // [START_EXCLUDE]
+          alert("Signed out");
+          // [END_EXCLUDE]
+        }
+        // [START_EXCLUDE]
+        document.getElementById('quickstart-sign-in').disabled = false;
+        // [END_EXCLUDE]
+      });
+      // [END authstatelistener]
 }
 function signInWithGoogle(){
 	firebase.auth().signInWithRedirect(googleProvider);
 }
 function signOut(){
 	firebase.auth().signOut().then(function() {
-	  alert("Signed out.");
 		location.reload();
 	}).catch(function(error) {
 	  // An error happened.
