7b95fbaabd9a46ba1c28d44f99e2251b08cddc69
timoman7 <tc20791@sestudent.org>
2017-05-03 11:27:24 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 var currentUser;
-var googleProvider = new firebase.auth.GoogleAuthProvider();
+var googleProvider;
 setInterval(function(){
 	if(currentUser){
 		var logoutbtn = $(".Logout");
@@ -13,39 +13,43 @@ setInterval(function(){
 		loginbtns.show();
 	}
 },100);
-firebase.auth().onAuthStateChanged(function(user) {
-	console.log(user);
-	if(user){
-		//Already logged in
-		currentUser = firebase.auth().currentUser;
-	}else{
-		firebase.auth().getRedirectResult().then(function(result) {
-			//Log in
-			var user = result.user;
-			var credential = result.credential;
-			if(user===null){
-				//User is null after redirect
-				firebase.auth().signInWithRedirect(googleProvider);
-			}else{
-				//User is defined after redirect
-				currentUser = firebase.auth().currentUser;
-			}
-		}, function(error) {
-			// The provider's account email, can be used in case of
-			// auth/account-exists-with-different-credential to fetch the providers
-			// linked to the email:
-			var email = error.email;
-			// The provider's credential:
-			var credential = error.credential;
-			// In case of auth/account-exists-with-different-credential error,
-			// you can fetch the providers using this:
-			console.log(error);
+function initApp(){
+	googleProvider = new firebase.auth.GoogleAuthProvider();
+	googleProvider.addScope('https://www.googleapis.com/auth/plus.login');
+	firebase.auth().getRedirectResult().then(function(result) {
+		//Log in
+		var user = result.user;
+		var credential = result.credential;
+		if(user===null){
+			//User is null after redirect
+		}else{
+			//User is defined after redirect
+			currentUser = firebase.auth().currentUser;
+		}
+	}, function(error) {
+		// The provider's account email, can be used in case of
+		// auth/account-exists-with-different-credential to fetch the providers
+		// linked to the email:
+		var email = error.email;
+		// The provider's credential:
+		var credential = error.credential;
+		// In case of auth/account-exists-with-different-credential error,
+		// you can fetch the providers using this:
+		console.log(error);
 
-		});
-	}
-},function(err){
-	console.log(err);
-});
+	});
+	firebase.auth().onAuthStateChanged(function(user) {
+		console.log(user);
+		if(user){
+			//Already logged in
+			currentUser = firebase.auth().currentUser;
+		}else{
+
+		}
+	},function(err){
+		console.log(err);
+	});
+}
 function signInWithGoogle(){
 	firebase.auth().signInWithRedirect(googleProvider);
 }
@@ -59,3 +63,6 @@ function signOut(){
 		alert("Somehow you screwed up logging out.");
 	});
 }
+window.onload = function(){
+	initApp();
+};
