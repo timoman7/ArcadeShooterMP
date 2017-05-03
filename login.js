3f715e605e4ae4f9493a2ce0da46c698a0984c0e
timoman7 <tc20791@sestudent.org>
2017-05-03 11:50:50 -0400

    Update login.js

--- login.js
+++ login.js
@@ -15,6 +15,37 @@ setInterval(function(){
 },100);
 function initApp(){
 	// Result from Redirect auth flow.
+	// [START getidptoken]
+      firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // [START_EXCLUDE]
+        } else {
+          // [END_EXCLUDE]
+        }
+        // The signed-in user info.
+        var user = result.user;
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // [START_EXCLUDE]
+        if (errorCode === 'auth/account-exists-with-different-credential') {
+          alert('You have already signed up with a different auth provider for that email.');
+          // If you are using multiple auth providers on your app you should handle linking
+          // the user's accounts here.
+        } else {
+          console.error(error);
+        }
+        // [END_EXCLUDE]
+      });
+      // [END getidptoken]
+	
       // Listening for auth state changes.
       // [START authstatelistener]
       firebase.auth().onAuthStateChanged(function(user) {
@@ -44,7 +75,7 @@ function initApp(){
 function signInWithGoogle(){
 	googleProvider = new firebase.auth.GoogleAuthProvider();
 	googleProvider.addScope('https://www.googleapis.com/auth/plus.login');
-	firebase.auth().signInWithPopup(googleProvider);
+	firebase.auth().signInWithRedirect(googleProvider);
 }
 function signOut(){
 	firebase.auth().signOut().then(function() {
